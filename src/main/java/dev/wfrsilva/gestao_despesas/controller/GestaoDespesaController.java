04389a54b2c850195628a35b1969c1369f8f07bf
Wendel Silva <wfrsilva@gmail.com>
2025-11-03 22:34:33 -0300

    Faltando metodo no controller para busca de email
    Falotu o metodo public List<Despesa> findByEmailAndDate no GestaoDespesaController.java
    
    Sem esse metodo nao eh possivel fazer buscas por email
    
    GET http://localhost:8080/gestao/v3@gmail.com
    
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
    	new file:   src/main/java/dev/wfrsilva/gestao_despesas/post.http

--- src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
@@ -1,10 +1,16 @@
 package dev.wfrsilva.gestao_despesas.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.wfrsilva.gestao_despesas.custom_message.ErrorMessage;
@@ -38,5 +44,11 @@ public class GestaoDespesaController {
         }//catch
 
     }//create
+
+    @GetMapping("/{email}")
+    public List <Despesa> findByEmailAndDate(@PathVariable String email, @RequestParam(required=false) LocalDate data)
+    {
+        return buscarDespesaUseCase.execute(email, data);
+    }//findByEmailAndDate
     
 }//GestaoDespesaController
